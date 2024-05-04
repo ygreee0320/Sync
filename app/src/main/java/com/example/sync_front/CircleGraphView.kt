cd9bd31978f96891a6b3b8ef19606c1dd9801fc9
bakhwee-bug <parksy8103@gmail.com>
2024-05-04 20:21:30 +0900

    [feat] 원형 그래프 숫자 애니메이션까지 구현

--- app/src/main/java/com/example/sync_front/CircleGraphView.kt
+++ app/src/main/java/com/example/sync_front/CircleGraphView.kt
@@ -6,6 +6,9 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.LinearInterpolator
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import java.util.ResourceBundle
 
 class CircleGraphView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -13,7 +16,8 @@ class CircleGraphView @JvmOverloads constructor(
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
-        strokeWidth = 80f // 원의 선 두께
+        strokeWidth = 110f // 원의 선 두께
+        textSize = 56f // 텍스트 크기 설정
     }
 
     private var sweepAngles = FloatArray(4) // 각 섹션의 각도 저장 배열
@@ -40,17 +44,51 @@ class CircleGraphView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val diameter = Math.min(width, height) - paint.strokeWidth
-        val rect = RectF(paint.strokeWidth, paint.strokeWidth, diameter, diameter)
+        val radius = (Math.min(width, height) / 2).toFloat()
+        val rect = RectF(
+            width / 2 - radius + paint.strokeWidth / 2, height / 2 - radius + paint.strokeWidth / 2,
+            width / 2 + radius - paint.strokeWidth / 2, height / 2 + radius - paint.strokeWidth / 2
+        )
 
         var startAngle = -90f
         val sectionColors =
             arrayOf(R.color.biscay_50, R.color.biscay_30, R.color.biscay_10, R.color.biscay_5)
 
+        val textColors =
+            arrayOf(R.color.white, R.color.biscay_70, R.color.biscay_50, R.color.biscay_30)
+
         for (i in 0 until 4) {
             paint.color = context.getColor(sectionColors[i])
             canvas.drawArc(rect, startAngle, sweepAngles[i], false, paint)
+
+            val percentage = (sweepAngles[i] / 360 * 100).toInt()
+            if (percentage > 0) { // 0%가 아닐 경우에만 텍스트를 그립니다.
+                val sectionCenterAngle = startAngle + sweepAngles[i] / 2
+                val sectionCenterX = (width / 2 + (radius - paint.strokeWidth / 2) * Math.cos(
+                    Math.toRadians(sectionCenterAngle.toDouble())
+                )).toFloat()
+                val sectionCenterY = (height / 2 + (radius - paint.strokeWidth / 2) * Math.sin(
+                    Math.toRadians(sectionCenterAngle.toDouble())
+                )).toFloat()
+
+                val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                    color = context.getColor(textColors[i])
+                    textAlign = Paint.Align.CENTER
+                    textSize = 40f
+                    typeface = ResourcesCompat.getFont(context, R.font.spoqahansansneo_bold)
+                }
+                val text = "${percentage}%"
+                canvas.drawText(
+                    text,
+                    sectionCenterX,
+                    sectionCenterY - (textPaint.descent() + textPaint.ascent()) / 2,
+                    textPaint
+                )
+            }
+
             startAngle += sweepAngles[i]
         }
     }
+
+
 }
