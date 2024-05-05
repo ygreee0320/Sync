7912d0c48be54b483b978495680e1fe72cd093eb
ygreee <ygreee0320@naver.com>
2024-05-06 02:27:41 +0900

    [add] 구글 로그인 버튼 추가

--- app/src/main/java/com/example/sync_front/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/sync_front/ui/login/LoginActivity.kt
@@ -10,12 +10,20 @@ import com.example.sync_front.BuildConfig.KAKAO_APP_KEY
 import com.example.sync_front.api_server.LoginManager
 import com.example.sync_front.api_server.Platform
 import com.example.sync_front.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    private val RC_GOOGLE_SIGN_IN = 1001 // 구글 로그인 요청 코드
+    private var googleApiClient: GoogleApiClient? = null // 구글 로그인 API 클라이언트
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +33,19 @@ class LoginActivity : AppCompatActivity() {
         // 저장된 토큰 꺼내기
         val sharedPreferences = getSharedPreferences("my_token", Context.MODE_PRIVATE)
         val accessToken = sharedPreferences.getString("access_token", null)
+
+        // GoogleSignInOptions 설정
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // GoogleApiClient 초기화
+        googleApiClient = GoogleApiClient.Builder(this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build()
+        googleApiClient?.connect()
 
         KakaoSdk.init(this, "${KAKAO_APP_KEY}")
 
@@ -61,12 +82,55 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        binding.loginBtn.setOnClickListener {
+        binding.kakaoLoginBtn.setOnClickListener {
             if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
                 UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
             } else {
                 UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
         }
+
+        binding.googleLoginBtn.setOnClickListener {
+            signInWithGoogle()
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient!!)
+        startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_GOOGLE_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+            handleGoogleSignInResult(result)
+        }
+    }
+
+    private fun handleGoogleSignInResult(result: GoogleSignInResult?) {
+        if (result?.isSuccess == true) {
+            val account = result.signInAccount
+            Log.d("my log", "$account")
+            saveAccessToken(account)
+            val idToken = account?.idToken
+            val accessToken = account?.serverAuthCode
+
+//            Log.d("my log", "$account")
+//            Log.d("my log", "$accessToken")
+            // 여기서 idToken을 사용하여 서버에 로그인 요청을 보내고 처리할 수 있습니다.
+        } else {
+            // Google 로그인 실패 처리
+        }
+    }
+
+    private fun saveAccessToken(account: GoogleSignInAccount?) {
+        val accessToken = account?.serverAuthCode // GoogleSignInAccount에서 액세스 토큰 가져오기
+
+        Log.d("my log 액세스", "$account")
+//        val sharedPreferences = getSharedPreferences("my_token", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//        editor.putString("google_access_token", accessToken)
+//        editor.apply()
     }
 }
