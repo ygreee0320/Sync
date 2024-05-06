315709e1707fe0124f1dd03294a7e43d214d5f94
ygreee <ygreee0320@naver.com>
2024-05-07 03:02:10 +0900

    [feat] 구글 로그인 연결

--- app/src/main/java/com/example/sync_front/api_server/ApiManager.kt
+++ app/src/main/java/com/example/sync_front/api_server/ApiManager.kt
@@ -1,6 +1,8 @@
 package com.example.sync_front.api_server
 
 import android.util.Log
+import com.example.sync_front.BuildConfig.GOOGLE_CLIENT_ID
+import com.example.sync_front.BuildConfig.GOOGLE_CLIENT_SECRET
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -25,6 +27,31 @@ object LoginManager {
             override fun onFailure(call: Call<LogInResponse>, t: Throwable) {
                 Log.e("서버 테스트", "오류2: ${t.message}")
                 callback(null)
+            }
+        })
+    }
+
+    fun getAccessToken(authCode:String, callback: (String?) -> Unit) { // 구글 액세스 토큰 요청
+        val apiService = GoogleClient().loginService
+
+        val call = apiService.getAccessToken(
+            request = LoginGoogleRequestModel(
+                grant_type = "authorization_code",
+                client_id = "${GOOGLE_CLIENT_ID}",
+                client_secret = "${GOOGLE_CLIENT_SECRET}",
+                code = authCode.orEmpty()
+            )
+        )
+        call.enqueue(object : Callback<LoginGoogleResponseModel> {
+            override fun onResponse(call: Call<LoginGoogleResponseModel>, response: Response<LoginGoogleResponseModel>) {
+                if(response.isSuccessful) {
+                    val accessToken = response.body()?.access_token.orEmpty()
+                    Log.d("서버 테스트", "accessToken: $accessToken")
+                    callback(accessToken) // 액세스 토큰 전달
+                }
+            }
+            override fun onFailure(call: Call<LoginGoogleResponseModel>, t: Throwable) {
+                Log.e("서버 테스트", "getOnFailure: ",t.fillInStackTrace() )
             }
         })
     }
--- app/src/main/java/com/example/sync_front/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/sync_front/ui/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.sync_front.BuildConfig.GOOGLE_CLIENT_ID
 import com.example.sync_front.ui.main.MainActivity
 import com.example.sync_front.BuildConfig.KAKAO_APP_KEY
 import com.example.sync_front.api_server.LoginManager
@@ -12,9 +13,9 @@ import com.example.sync_front.api_server.Platform
 import com.example.sync_front.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.GoogleApiClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
@@ -22,7 +23,7 @@ import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
-    private val RC_GOOGLE_SIGN_IN = 1001 // 구글 로그인 요청 코드
+    private val GOOGLE_LOGIN = 1000 // 구글 로그인 요청 코드
     private var googleApiClient: GoogleApiClient? = null // 구글 로그인 API 클라이언트
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +38,8 @@ class LoginActivity : AppCompatActivity() {
         // GoogleSignInOptions 설정
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
+            .requestServerAuthCode("${GOOGLE_CLIENT_ID}")
+            .requestIdToken("${GOOGLE_CLIENT_ID}")
             .build()
 
         val googleSignInClient = GoogleSignIn.getClient(this, gso)
@@ -50,35 +53,13 @@ class LoginActivity : AppCompatActivity() {
         KakaoSdk.init(this, "${KAKAO_APP_KEY}")
 
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                // 소셜 로그인 실패
+            if (error != null) { // 카카오 소셜 로그인 실패
                 Log.d("my log", "로그인 실패")
-            } else if (token != null) {
-                // 소셜 로그인 성공
+            } else if (token != null) { // 소셜 로그인 성공
                 Log.d("my log", "로그인 성공 - 토큰 값 :" + token.accessToken)
 
-                // 토큰 저장
-                val sharedPreferences = this.getSharedPreferences("my_token", Context.MODE_PRIVATE)
-                val editor = sharedPreferences.edit()
-
                 // 서버에 로그인 전송
-                LoginManager.sendLogin(
-                    token.accessToken, Platform("kakao")
-                ) { response ->
-                    response?.let {
-                        Log.d("my log", "서버 - 로그인 성공")
-
-                        editor.putString("access_token", token.accessToken) // 액세스 토큰 저장
-                        editor.putString("email", response.email)
-                        editor.putString("name", response.name)
-                        editor.apply()
-
-                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-                        finish()
-                    }
-                }
-
+                sendLoginToServer("kakao", token.accessToken)
             }
         }
 
@@ -91,46 +72,66 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.googleLoginBtn.setOnClickListener {
-            signInWithGoogle()
+            onLogin(googleSignInClient)
         }
     }
 
-    private fun signInWithGoogle() {
-        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient!!)
-        startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
+    fun onLogin(client: GoogleSignInClient) {
+        val signInIntent: Intent = client.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_GOOGLE_SIGN_IN) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
-            handleGoogleSignInResult(result)
+        when(requestCode) {
+            GOOGLE_LOGIN -> {
+                try {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    Log.d("my log", "$task")
+
+                    val account = task.getResult(ApiException::class.java)
+                    val authCode = task.getResult(ApiException::class.java)?.serverAuthCode
+                    Log.d("my log", "$authCode")
+
+                    Log.d("my log", account.email ?: "")
+                    Log.d("my log", "id 토큰 ${account.idToken}")
+
+                    LoginManager.getAccessToken(authCode!!){ response ->
+                        response?.let {
+                            Log.d("my log", "$response")
+
+                            sendLoginToServer("google", response)
+                        }
+                    }
+
+                } catch (e: ApiException) {
+                    e.printStackTrace()
+                    Log.d("my log", "구글 로그인 실패")
+                }
+            }
         }
     }
 
-    private fun handleGoogleSignInResult(result: GoogleSignInResult?) {
-        if (result?.isSuccess == true) {
-            val account = result.signInAccount
-            Log.d("my log", "$account")
-            saveAccessToken(account)
-            val idToken = account?.idToken
-            val accessToken = account?.serverAuthCode
+    private fun sendLoginToServer(platform: String, accessToken: String) {
+        LoginManager.sendLogin(
+            accessToken, Platform(platform)
+        ) { response ->
+            response?.let {
+                Log.d("my log", "서버 - 로그인 성공")
 
-//            Log.d("my log", "$account")
-//            Log.d("my log", "$accessToken")
-            // 여기서 idToken을 사용하여 서버에 로그인 요청을 보내고 처리할 수 있습니다.
-        } else {
-            // Google 로그인 실패 처리
+                // 토큰 저장
+                val sharedPreferences = this.getSharedPreferences("my_token", Context.MODE_PRIVATE)
+                val editor = sharedPreferences.edit()
+
+                editor.putString("access_token", accessToken) // 액세스 토큰 저장
+                editor.putString("email", response.email)
+                editor.putString("name", response.name)
+                editor.apply()
+
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
+            }
         }
-    }
-
-    private fun saveAccessToken(account: GoogleSignInAccount?) {
-        val accessToken = account?.serverAuthCode // GoogleSignInAccount에서 액세스 토큰 가져오기
-
-        Log.d("my log 액세스", "$account")
-//        val sharedPreferences = getSharedPreferences("my_token", Context.MODE_PRIVATE)
-//        val editor = sharedPreferences.edit()
-//        editor.putString("google_access_token", accessToken)
-//        editor.apply()
     }
 }
