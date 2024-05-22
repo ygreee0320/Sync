39e9479a61de1777d117a5bb6c0ad0569a1eaf04
bakhwee-bug <parksy8103@gmail.com>
2024-05-22 14:30:10 +0900

    [chore] 파일명 오타 수정

--- app/src/main/java/com/example/sync_front/ui/open/OpenLeaderFragment.kt
+++ app/src/main/java/com/example/sync_front/ui/open/OpenLeaderFragment.kt
@@ -1,45 +1,20 @@
 package com.example.sync_front.ui.open
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.util.Log
-import retrofit2.Call
-import retrofit2.Response
-import android.content.Intent
 import android.net.Uri
-import android.widget.Toast
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.net.toUri
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.sync_front.R
-import com.example.sync_front.api_server.RetrofitClient
 import com.example.sync_front.databinding.FragmentOpenReaderBinding
-import com.example.sync_front.ui.main.MainActivity
-import com.example.sync_front.data.model.OpenSync
-import com.example.sync_front.data.model.PostOpenSync
 import com.example.sync_front.data.model.SharedOpenSyncData
-import com.example.sync_front.data.service.OpenResponse
-import com.google.gson.Gson
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import retrofit2.Callback
-import java.io.File
-import java.io.FileOutputStream
-import id.zelory.compressor.Compressor
-import id.zelory.compressor.constraint.quality
-import id.zelory.compressor.constraint.resolution
-import kotlinx.coroutines.launch
 
-class OpenReaderFragment : Fragment() {
+class OpenLeaderFragment : Fragment() {
     private var _binding: FragmentOpenReaderBinding? = null
     private val binding get() = _binding!!
     private val openViewModel: OpenViewModel by activityViewModels()
