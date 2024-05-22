dd36ad278b658484195321023c25140f42ba826b
bakhwee-bug <parksy8103@gmail.com>
2024-05-23 03:02:12 +0900

    [feat] 지속성 싱크 개설 ui랑 중복 선택 불가

--- app/src/main/java/com/example/sync_front/ui/open/OpenRoutineFragment.kt
+++ app/src/main/java/com/example/sync_front/ui/open/OpenRoutineFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.sync_front.R
@@ -16,6 +17,8 @@ class OpenRoutineFragment : Fragment() {
     private var _binding: FragmentOpenRoutineBinding? = null
     private val binding get() = _binding!!
     private val openViewModel: OpenViewModel by activityViewModels()
+    private var selectedTextView: TextView? = null
+
 
 
     override fun onCreateView(
@@ -50,7 +53,28 @@ class OpenRoutineFragment : Fragment() {
             binding.doneBtn.setBackgroundResource(R.drawable.btn_gray_10)
         }
     }
+
+    private fun toggleTextViewSelection(textView: TextView) {
+        if (selectedTextView == textView) {
+            // 이미 선택된 TextView를 클릭한 경우
+            textView.isSelected = false
+            selectedTextView = null
+        } else {
+            // 새로운 TextView를 클릭한 경우
+            selectedTextView?.isSelected = false // 이전에 선택된 TextView의 선택 해제
+            textView.isSelected = true // 선택된 TextView로 설정
+            selectedTextView = textView
+        }
+    }
+
     private fun setupClickListeners() {
+        binding.dateText1.setOnClickListener { toggleTextViewSelection(binding.dateText1) }
+        binding.dateText2.setOnClickListener { toggleTextViewSelection(binding.dateText2) }
+        binding.dateText3.setOnClickListener { toggleTextViewSelection(binding.dateText3) }
+        binding.dateText4.setOnClickListener { toggleTextViewSelection(binding.dateText4) }
+        binding.dateText5.setOnClickListener { toggleTextViewSelection(binding.dateText5) }
+        binding.dateText6.setOnClickListener { toggleTextViewSelection(binding.dateText6) }
+        binding.dateText7.setOnClickListener { toggleTextViewSelection(binding.dateText7) }
         binding.beforeBtn.setOnClickListener {
             findNavController().navigateUp() // 이전 프래그먼트로
         }
