3fcba712dd96873d60cc1bf9f3badd93fd65a8f5
bakhwee-bug <parksy8103@gmail.com>
2024-05-23 20:45:21 +0900

    [style] 싱크 개설 인원 애니메이션 추가

--- app/src/main/java/com/example/sync_front/ui/open/OpenCntFragment.kt
+++ app/src/main/java/com/example/sync_front/ui/open/OpenCntFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.activityViewModels
 import com.example.sync_front.R
@@ -39,6 +40,7 @@ class OpenCntFragment : Fragment() {
         setUpChangedListener()
         observeViewModel()
         setupClickListeners()
+        startEntryAnimations()
     }
 
     private fun observeViewModel() {
@@ -129,6 +131,35 @@ class OpenCntFragment : Fragment() {
             requireActivity().finish()
         }
 
+    }
+
+    private fun startEntryAnimations() {
+        val initialTranslationY = 300f
+        val duration = 1000L
+        val interpolator = AccelerateDecelerateInterpolator()
+
+        binding.tv1.apply {
+            translationY = initialTranslationY
+            alpha = 0f
+            animate()
+                .translationY(0f)
+                .alpha(1f)
+                .setInterpolator(interpolator)
+                .setDuration(duration)
+                .start()
+        }
+
+        binding.tv2.apply {
+            translationY = initialTranslationY
+            alpha = 0f
+            animate()
+                .translationY(0f)
+                .alpha(1f)
+                .setInterpolator(interpolator)
+                .setDuration(duration)
+                .setStartDelay(200L)
+                .start()
+        }
     }
 
     override fun onDestroyView() {
